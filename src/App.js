d7a45acd277c6f110dfaf379997456e3e55d7d8d
Damith08 <dmthsamarasinghe@gmail.com>
2024-01-14 18:34:11 +0530

    feat: remove hard corded data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import data from "./data";
 
 export default function App() {
+  const body = data.map((item) => {
+    return <Body key={item.id} item={item} />;
+  });
+
   return (
     <>
       <Header />
-      <Body />
+      <section>{body}</section>
     </>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import LOCATION from "../assets/images/Fill 219.svg";
 
-export default function Body() {
+export default function Body(props) {
   return (
     <div className="card">
       <section className="card--section">
         <div>
           <img
-            src="https://source.unsplash.com/WLxQvbMyfas"
+            src={props.item.imageUrl}
             alt="mount-fuji"
             className="card--image"
           ></img>
@@ -18,16 +18,12 @@ export default function Body() {
               alt="location-img"
               className="location-img"
             ></img>
-            <p className="location--text">JAPAN</p>
+            <p className="location--text">{props.item.location}</p>
             <p className="google-map--link">View on Google Maps</p>
           </div>
-          <h1 className="card--heading">Mount Fuji</h1>
-          <h5 className="card--date">12 Jan, 2021 - 24 Jan, 2021</h5>
-          <p className="card--description">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <h1 className="card--heading">{props.item.title}</h1>
+          <h5 className="card--date">{props.item.data}</h5>
+          <p className="card--description">{props.item.description}</p>
         </div>
       </section>
       <hr />
